b5f9e8675a724757cff2583476cca4e3bb05a0e6
Md. Abdur Rahman Jyoti <Jyoti@jyotis-macbook-pro.local>
2017-04-10 20:23:08 +0600

    Initial Commit

---
+++ git_reo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_reo
+//
+//  Created by Md. Abdur Rahman Jyoti on 4/10/17.
+//  Copyright © 2017 Md. Abdur Rahman Jyoti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
